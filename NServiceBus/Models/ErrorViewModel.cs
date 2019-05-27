7b29f09de7221d6d6430b9a01288cb46b49b387d
CelaniN <CelaniN@CNU437984Z>
2019-05-27 13:58:46 +0200

    Add project files.

---
+++ NServiceBus/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NServiceBus.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
